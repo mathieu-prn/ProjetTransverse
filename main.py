d4d110507c19a172f6fd426a2aa8f9ebe7c1d899
FlorianRibrioux <florian.ribrioux@efrei.net>
2025-02-03 11:27:17 +0100

    Added Fullscreen to the main fill

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pygame
 print(pygame.init())
 
 # Set up the game window
-WIDTH = 1000
-HEIGHT = 500
+WIDTH = 1500
+HEIGHT = 700
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("EfreiSport")
 
@@ -16,13 +16,20 @@ blue=(0,0,255)
 
 
 clock = pygame.time.Clock()
-
+fullscreen=False
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key== pygame.K_f:
+                fullscreen=not fullscreen
+                if fullscreen:
+                    screen=pygame.display.set_mode((WIDTH,HEIGHT),pygame.FULLSCREEN)
+                else:
+                    screen=pygame.display.set_mode((WIDTH, HEIGHT))
     screen.fill(blue)
 
     # Update the display
