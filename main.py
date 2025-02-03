836615b7ba459ecabc89769a66a8f758e7fba83d
Mathieu Prince <mathieu1.prince1@gmail.com>
2025-02-03 11:09:12 +0100

    added basic code in main

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+
+# Initialize Pygame
+print(pygame.init())
+
+# Set up the game window
+WIDTH = 1000
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("EfreiSport")
+
+# Initialize Colors
+red=(255,0,0)
+green=(0,255,0)
+blue=(0,0,255)
+
+
+clock = pygame.time.Clock()
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.fill(blue)
+
+    # Update the display
+    pygame.display.flip()
+
+    # Set the frame rate
+    clock.tick(60)
+
+# Quit the game
+pygame.quit()
