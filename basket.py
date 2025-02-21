588a180ea107be585142331911877d1bd75ec4d5
theokng <theo.konig@efrei.net>
2025-02-21 18:05:03 +0100

    basket: adding slider & go button + reorganising classes

--- basket.py
+++ basket.py
@@ -22,7 +22,6 @@ grey=(211,211,211)
 # Initialize constants
 G = 9.81
 dt = 1/10
-t = 0
 PI = math.pi
 bounce_coeff = 0.7
 
@@ -34,17 +33,21 @@ class Ball(pygame.sprite.Sprite):
         img = pygame.image.load("assets/Basket/BasketBall.png")
         self.image = pygame.transform.scale(img, (50, 50))
         self.rect = self.image.get_rect()
-        self.rect.center = (50, 400)
+        self.rect.center = (100, 400)
         self.scored=False
+        self.velocity = 0
+        self.x_coeff = (0, self.rect.center[0])
+        self.y_coeff = (0, 0, self.rect.center[1])
+        self.time = 0
+        self.launched = False
 
     def trajectory_equation(self, speed, angle, x0, y0):
         v_x = speed * math.cos(angle * math.pi/180)
         v_y = speed * math.sin(angle * math.pi/180)
-        x = (v_x, x0)
-        y = (0.5 * G, -v_y, y0)
-        return x, y
+        self.x_coeff = (v_x, x0)
+        self.y_coeff = (0.5 * G, -v_y, y0)
 
-    def collision(self):
+    def hoop_collision(self):
         if self.rect.colliderect(hoop_detector.rect):
             if not self.scored:
                 score.increment()
@@ -52,10 +55,14 @@ class Ball(pygame.sprite.Sprite):
         if not self.rect.colliderect(hoop_detector.rect):
             self.scored = False
 
-    def calc_pos(self, x_coeff, y_coeff, time):
-        new_x = x_coeff[0]*time + x_coeff[1]
-        new_y = y_coeff[0]*(time**2) + y_coeff[1]*time + y_coeff[2]
-        return (new_x, new_y)
+    def ground_collision(self):
+        if self.rect.colliderect(borderbottom.rect):
+            return True
+
+    def update_pos(self):
+        self.rect.center = self.x_coeff[0]*self.time + self.x_coeff[1], self.y_coeff[0]*(self.time**2) + self.y_coeff[1]*self.time + self.y_coeff[2]
+        self.velocity = math.sqrt((math.cos(55) * self.velocity) ** 2 + (G * self.time + math.sin(55) * self.velocity) ** 2)
+        self.time += dt
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
@@ -130,16 +137,69 @@ class Scene:
         for obj in self.objects:
             obj.draw(surface)
 
+class Slider(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.rect = pygame.Rect(25, 150, 30, 200)
+        self.slider_rect = pygame.Rect(25, 250, 30, 7)
+        self.dragging = False
+
+    def draw(self, surface):
+        pygame.draw.rect(surface, blue_efrei, self.rect.inflate(6, 6))
+        pygame.draw.rect(surface, white, self.rect)
+        pygame.draw.rect(surface, black, self.slider_rect)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.slider_rect.collidepoint(event.pos):
+                self.dragging = True
+            elif self.rect.collidepoint(event.pos):
+                self.slider_rect.y = max(self.rect.top, min(event.pos[1], self.rect.bottom - self.slider_rect.height))
+                self.dragging = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.dragging = False
+        elif event.type == pygame.MOUSEMOTION and self.dragging:
+            self.slider_rect.y = max(self.rect.top, min(event.pos[1], self.rect.bottom - self.slider_rect.height))
+
+    def get_value(self):
+        min_y = self.rect.top
+        max_y = self.rect.bottom - self.slider_rect.height
+        return int(100 - (((self.slider_rect.y - min_y) / (max_y - min_y)) * 100)) + int(10 * ((self.slider_rect.y - min_y) / (max_y - min_y)))
+
+class Launch(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.rect = pygame.Rect(17, 396, 50, 80)
+        self.color = white
+
+    def draw(self, surface=screen):
+        pygame.draw.rect(surface, blue_efrei, self.rect.inflate(6, 6))
+        pygame.draw.rect(surface, self.color, self.rect)
+        font = pygame.font.Font(None, 45)
+        text = font.render("Go!", True, blue_efrei)
+        surface.blit(text, (self.rect.x, self.rect.y + 30))
+
+    def clicked(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.rect.collidepoint(event.pos):
+                self.color = grey
+                ball.velocity = (slider.get_value()/100) * 120
+                ball.trajectory_equation(ball.velocity, 50, ball.rect.center[0], ball.rect.center[1])
+                ball.launched = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.color = white
+
 #Object initialization
 ball = Ball()
 hoop = Hoop()
 score=Score()
+slider = Slider()
+launch_button = Launch()
 hoop_detector= Hoop_detector(792,220)
 bordertop=Border(0,0,900,6,True)
 borderleft=Border(0,0,6,425,True)
 borderbottom=Border(0,425,900,6,True)
 borderright=Border(900,0,6,431,True)
-calc_eq = True
 
 # Create the scene and add the walls
 scene = Scene()
@@ -148,50 +208,39 @@ scene.add_object(borderleft)
 scene.add_object(borderright)
 scene.add_object(borderbottom)
 
-
-v_now = 100
+ball.velocity = 90
 angle = 50
-final_pos =()
 # Game loop
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        slider.handle_event(event)
+        launch_button.clicked(event)
 
     screen.fill((240, 240, 240, 0.5))
     screen.blit(bg, (0, 0))
 
-    if calc_eq:
-        x_coeff, y_coeff = ball.trajectory_equation(v_now, angle, ball.rect.center[0], ball.rect.center[1])
-        #speed should be between 50 and 150
-        calc_eq = False
+    if ball.ground_collision():
+        ball.trajectory_equation(bounce_coeff*ball.velocity, angle, ball.rect.center[0], ball.rect.center[1])
+        ball.time = dt
 
-    v_now = math.sqrt((math.cos(55) * v_now)**2 + (G * t + math.sin(55) * v_now)**2)
+    if ball.launched:
+        ball.update_pos()
 
-    if ball.rect.center[1]>=450 and t>0.5 :
-        x_coeff, y_coeff = ball.trajectory_equation(bounce_coeff*v_now, angle, ball.rect.center[0], ball.rect.center[1])
-        t = 0
+    ball.hoop_collision()
 
-
-    if final_pos == ():
-        if v_now < 0.1:
-            final_pos = (ball.rect.center[0], ball.rect.center[1])
-        else:
-            ball.rect.center = ball.calc_pos(x_coeff, y_coeff, t)
-    else:
-        ball.rect.center = final_pos
-
-    ball.collision()
     hoop_detector.draw(screen)
     ball.draw(screen)
     hoop.draw(screen)
     scene.draw(screen)
     score.draw(screen)
+    slider.draw(screen)
+    launch_button.draw()
 
     # Update the display
     pygame.display.flip()
-    t+=dt
     # Set the frame rate
     clock.tick(60)
 
