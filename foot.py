9cbabde6b0948614a60a6d5f27f982ce9d2c3d60
BenjaminBentze <benjamin.bentze@efrei.net>
2025-03-17 12:03:16 +0100

    better values of slider

---
+++ foot.py
@@ -0,0 +1,40 @@
+import pygame, math
+
+# Initialize Pygame
+pygame.init()
+
+# Set up the game window
+WIDTH = 1000
+HEIGHT = 500
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("EfreiSport - Football")
+bg = pygame.image.load("assets/Common/Background.png")
+pygame_icon = pygame.image.load('assets/Common/logo.png')
+pygame.display.set_icon(pygame_icon)
+
+# Initialize Colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+red=(255,0,0)
+blue_efrei=(18,121,190)
+grey=(211,211,211)
+
+clock = pygame.time.Clock()
+
+#scene = Scene()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((240, 240, 240, 0.5))
+    screen.blit(bg, (0, 0))
+
+    pygame.display.flip()
+    # Set the frame rate
+    clock.tick(60)
+
+# Quit the game
+pygame.quit()
